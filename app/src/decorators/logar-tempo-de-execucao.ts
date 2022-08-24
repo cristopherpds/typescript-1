7e24cff566c450050f517e0aa4d7e7d35aa03e41
Cristopher Paiva <josuepaiva7@gmail.com>
2022-08-24 12:49:33 -0300

    Trabalhando com decorator com parametro, criacao de decorator de inspecao, simpificacao no design de decorators

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,19 +1,26 @@
-export function logarTempoDeExecucao(){
-  return function(
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
+  return function (
     target: any,
     propertyKey: string,
     descriptor: PropertyDescriptor
-  ){
+  ) {
     const metodoOriginal = descriptor.value;
-    descriptor.value = function(...args: any[]){
+    descriptor.value = function (...args: any[]) {
+      let divisor = 1;
+      let unidade = "milisegundos";
+      if (emSegundos) {
+        divisor = 1000;
+        unidade = "segundos";
+      }
       const t1 = performance.now();
       const retorno = metodoOriginal.apply(this, args);
       const t2 = performance.now();
-      console.log(`${propertyKey}, tempo de execucao:${(t2 - t1)/1000}
-      segundos`);
-      retorno
-      
-    }
+      console.log(
+        `${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} ${unidade}`
+      );
+      return retorno;
+    };
+
     return descriptor;
-  }
+  };
 }
